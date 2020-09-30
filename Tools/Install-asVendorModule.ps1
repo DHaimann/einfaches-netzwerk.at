086d99ab34ab7ae43b624c7418f87fd66a439e8a
Dietmar Haimann <58327672+DHaimann@users.noreply.github.com>
2020-09-30 09:21:46 +0200

    Add files via upload
    Install or update PowerShell module from HP (HPCMSL) or Dell (DellBIOSProvider) to use for ConfigMgr CI.

---
+++ Tools/Install-asVendorModule.ps1
@@ -0,0 +1,49 @@
+﻿Function Install-asVendorModule {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ModuleName
+    )
+
+    BEGIN {
+        $null = netsh winhttp set proxy proxy-server="proxy.ooe.gv.at:8080" bypass-list="<local>"
+    }
+    PROCESS {
+    
+        $NuGetLocal = Get-PackageProvider -ListAvailable | Where-Object -Property Name -EQ NuGet
+
+        If ($null -eq $NuGetLocal) {
+            Install-PackageProvider -Name 'NuGet' -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
+            Install-Module -Name 'PowerShellGet' -Force -Scope AllUsers -AllowClobber -SkipPublisherCheck
+        }
+
+        $ModuleOnline = Find-Module -Name $ModuleName -Repository 'PSGallery'
+        [Version]$ModuleOnlineVersion = $ModuleOnline.Version
+
+        Try {
+            $ModuleLocal = Get-InstalledModule -Name $ModuleName -ErrorAction Stop
+            [Version]$ModuleLocalVersion = $ModuleLocal.Version
+
+            If ($ModuleLocalVersion -ne $ModuleOnlineVersion) {
+                Update-Module -Name $ModuleName -Force
+            }
+        }
+        Catch {
+            Install-Module -Name $ModuleName -Repository 'PSGallery' -Force -Scope AllUsers -SkipPublisherCheck -AcceptLicense
+        }
+    }
+    END {
+        $null = netsh winhttp reset proxy
+    }    
+}
+
+$Vendor = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
+
+If ($Vendor -like "H*P*") {
+    $ModuleName = "HPCMSL"
+} ElseIf ($Vendor -like "Dell*") {
+    $ModuleName = "DellBIOSProvider"
+}
+
+Install-asVendorModule -ModuleName $ModuleName
